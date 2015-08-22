3586d4685e52d4439ad63492ce3d4efb831aa0f6
Matthew Karas <matt@mattkaras.info>
2015-08-22 00:02:39 -0400

    Update to use the free store for the arrays

--- main.cpp
+++ main.cpp
@@ -9,17 +9,20 @@ auto constexpr bigNum = 100000;
 
 auto TimeToCopyArray () {
     // array
-    int dstArr[bigNum] {};
-    int srcArr[bigNum] {};
+    int* dstArr = new int [bigNum];
+    int* srcArr = new int [bigNum];
     for (auto ii = 0; ii < bigNum; ++ii ) {
         srcArr[ii] = ii;
+        dstArr[ii] = 0;
     }
     assert(dstArr[3] == 0);
     auto ts1 = std::chrono::high_resolution_clock::now();
-    memcpy(dstArr,srcArr, sizeof(srcArr));
+    memcpy(dstArr,srcArr, sizeof(int)*bigNum);
     auto ts2 = std::chrono::high_resolution_clock::now();
     assert(dstArr[3] == 3);
     auto dur = std::chrono::duration_cast<std::chrono::microseconds>(ts2-ts1).count();
+    delete[] dstArr;
+    delete[] srcArr;
     return dur;
 }
 
