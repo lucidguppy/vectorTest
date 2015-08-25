aee8e56eb859b2d77acaba3fa1315c755a4d7e3c
Chris Morgan <cmorgan@cybexintl.com>
2015-08-24 22:08:08 -0400

    process.py support saving figures to .svg. generate.sh, save the series of images for each buffer size

--- generate.sh
+++ generate.sh
@@ -1,11 +1,22 @@
-./vectorTest 64 6000000 > data64.csv
-./vectorTest 128 6000000 > data128.csv
-./vectorTest 256 3000000 > data256.csv
-./vectorTest 1024 1500000 > data1025.csv
-./vectorTest 2048 600000 > data2048.csv
-./vectorTest 4096 300000 > data4096.csv
-./vectorTest 8192 100000 > data8192.csv
-./vectorTest 16384 80000 > data16384.csv
-./vectorTest 32768 40000 > data32768.csv
-./vectorTest 65535 20000 > data65535.csv
-./vectorTest 1048576 10000 > data1048576.csv
+./vectorTest 64 10000000 > data64.csv
+./vectorTest 128 8000000 > data128.csv
+./vectorTest 256 4000000 > data256.csv
+./vectorTest 1024 2000000 > data1024.csv
+./vectorTest 2048 1000000 > data2048.csv
+./vectorTest 4096 700000 > data4096.csv
+./vectorTest 8192 500000 > data8192.csv
+./vectorTest 16384 300000 > data16384.csv
+./vectorTest 32768 200000 > data32768.csv
+./vectorTest 65535 100000 > data65535.csv
+./vectorTest 1048576 70000 > data1048576.csv
+
+python ./process.py data64.csv save
+python ./process.py data128.csv save
+python ./process.py data256.csv save
+python ./process.py data1024.csv save
+python ./process.py data2048.csv save
+python ./process.py data4096.csv save
+python ./process.py data8192.csv save
+python ./process.py data16384.csv save
+python ./process.py data65535.csv save
+python ./process.py data1048576.csv save
--- process.py
+++ process.py
@@ -8,7 +8,10 @@ matplotlib.style.use('ggplot')
 df = pd.read_csv(sys.argv[1])
 
 #df.hist(bins=30)
-df.plot(widths=0.8, notch=False, kind='box', sym='r+')
+ax = df.plot(widths=0.8, notch=False, kind='box', sym='r+')
 
-plt.show()
-
+if sys.argv[2] == "save":
+    fig = ax.get_figure()
+    fig.savefig(sys.argv[1] + ".svg")
+else:    
+    plt.show()
