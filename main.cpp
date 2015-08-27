9c222da304bfb7af183bee549c1fca43c9ec5c95
Chris Morgan <cmorgan@cybexintl.com>
2015-08-26 21:12:17 -0400

    Add to the output figure the percentage difference between vector and array in terms of average timing

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <utility>
 #include <ctime>
 #include <cstdlib>
+#include <iomanip>
 
 // Define VERIFY to perform some asserts.
 // Is disabled by default to simplify the
@@ -23,9 +24,44 @@ int loopNum;
 int main(int argc, char* argv[]) {
     theBigNum = atoi(argv[1]);
     loopNum = atoi(argv[2]);
-    std::cout << "ArrayCopy(dur us) - " << theBigNum << " bytes, VectorCopy(dur us) - " << theBigNum << " bytes\n";
+
+    std::vector<std::pair<int,int>> array;
+    std::vector<std::pair<int,int>> vector;
+
     for (int ii = 0; ii < loops; ++ii) {
-        std::cout << TimeToCopyArray(theBigNum, loopNum).first << "," << TimeToCopyVector(theBigNum, loopNum).first << "\n";
+        array.push_back(TimeToCopyArray(theBigNum, loopNum));
+        vector.push_back(TimeToCopyVector(theBigNum, loopNum));
     }
+
+    // sum the times
+    auto arrayTotal = 0;
+    for(auto e : array)
+    {
+        arrayTotal += e.first;
+    }
+
+    auto vectorTotal = 0;
+    for(auto e : vector)
+    {
+        vectorTotal += e.first;
+    }
+
+    // calculate the averages
+    auto arrayAverage = arrayTotal / (double)array.size();
+    auto vectorAverage = vectorTotal / (double)vector.size();
+
+    // calculate the time relative to the vector
+    auto vectorTimePercentage = (vectorAverage - arrayAverage) / vectorAverage;
+    auto vectorDeltaString = (vectorTimePercentage > 0) ? "slower" : "faster";
+
+    std::ios_base::fmtflags originalFlags = std::cout.flags();
+    std::cout << "ArrayCopy(dur us) - " << theBigNum << " bytes, VectorCopy(dur us) - "
+        << theBigNum << " bytes. Vector " << vectorDeltaString << " by "
+        << std::fixed << std::setprecision(1) << (vectorTimePercentage * 100.0) << "%\n";
+    std::cout.flags( originalFlags );
+    for (int ii = 0; ii < loops; ++ii) {
+        std::cout << array[ii].first << "," << vector[ii].first << "\n";
+    }
+
     return 0;
 }
